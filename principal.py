3257dc5afc4ad6354f55e147950e17ec3109ad65
jcph1 <69978753+jcph1@users.noreply.github.com>
2022-04-04 12:37:48 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,15 +1,41 @@
+import faltas,golazos,timer
+
+
 menu="""
 Partido numero: x
 1.Ingrese el nombre del equipo1:         Ingrese el nombre del equipo2:
-2.Ingrese el nombre del juez:
-3.Hora de inicio del partido:
-4.Hora de final del partido:
+2.Hora de inicio del partido:
+3.Ingrese falta(si hubo falta):
+4.Ingrese golazo:
 
-
-
-    
+    5.Resultados del partido:acabe el temporizador
 
 
 """
+menu=int(input)
 
-#while inicio_hora > 
+#numeral 1 
+equipo1=input("Equipo local")
+equipo2=input("Equipo visitante")
+print(equipo1, "vs",equipo2)
+#numeral 2 
+timer.iniciar()
+
+#numeral 3
+falta_penalizacion=faltas.falta_penalizacion()
+faltas.misconduct(falta_penalizacion)    
+
+#numeral 4
+equipo=input("Que equipo hizo el gol?: ")
+if equipo=="visitante":
+    golazos.gol2()
+elif equipo=="local":
+    golazos.gol1()
+    
+
+#numeral 5
+print("marcador final:","local",golazos.gol1(),":","visitante",golazos.gol2())
+print("")
+
+
+
