c4b033d8b57d2f38c2a3e649489d4d530df3e6ed
jcph1 <69978753+jcph1@users.noreply.github.com>
2022-04-04 11:33:45 -0500

    Add files via upload

---
+++ principal.py
@@ -0,0 +1,15 @@
+menu="""
+Partido numero: x
+1.Ingrese el nombre del equipo1:         Ingrese el nombre del equipo2:
+2.Ingrese el nombre del juez:
+3.Hora de inicio del partido:
+4.Hora de final del partido:
+
+
+
+    
+
+
+"""
+
+#while inicio_hora > 
