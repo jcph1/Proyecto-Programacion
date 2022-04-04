d0f6b0eee113cbc78f27efd766e9b5ed8fb2c2af
jcph1 <69978753+jcph1@users.noreply.github.com>
2022-04-04 12:37:12 -0500

    Update faltas.py

--- faltas.py
+++ faltas.py
@@ -1,33 +1,34 @@
 falta_menor=["intento ataque", "broken stick", "charging", "boarding", "cross checking", "delaying", "elbowing", " falling on the puck","handpass", "high sticks", "holding", "hoocking","interference","slashing","tripping" ]
 falta_mayor=["fighting","interference by spectators", "checking from behind", 'leaving penalty bench', "abuse of officials", "profane language", ]
-jugador_1= int(input("Ingrese el numero de jersey del jugador: "))
+jugador_1= int(input("Ingrese el numero de jersey del jugador penalizado: "))
 
 
 def falta_penalizacion():
     x=input("Ingrese el nombre de la falta: ")
-    falta=0
+    faltas=0
 
     if x in falta_menor:
         print("Debe permanecer durante 2 minutos en el penalty box")
-        falta+=1
+        faltas+=1
         
     elif x in falta_mayor:
         print("Debe permanecer durante 10 minutos en el penalty box")
-        falta+=3
+        faltas+=3
         
     elif x not in falta_menor or x not in falta_mayor:
         print("la falta no existe o esta escrita de forma incorrecta")
     
-    return falta
+    return faltas
 
 
 def misconduct(falta_penalizacion):
     
     if falta_penalizacion < 2:
-        print("el jugador no esta en riesgo de misconduct")    
+        print("el jugador (format) no esta en riesgo de misconduct")    
     else:
-        print("se debe de expulsar al jugador del partido")
- 
+        print("el jugador (format) se debe de expulsar al jugador del partido")
         
-falta_penalizacion()
-misconduct(falta_penalizacion())    
+
+
+#hacer funcion que sume todas las faltas de los jugadores dependiendo del equipo en el que esten 
+        
